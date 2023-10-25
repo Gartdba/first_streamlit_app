234b418f78ca1a81754ed70b9baa429da57fd1da
Gartdba <147558955+Gartdba@users.noreply.github.com>
2023-10-25 16:16:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,14 +34,14 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
 streamlit.header("Fruityvice Fruit Advice")
 ##streamlit.text(fruityvice_response.json()) # just writes the data to the screen
 
-# write your own comment - put the data from fruityvice.com/api into new variable named below but formats it.
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice")
 #fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
 streamlit.write('The user entered', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+# write your own comment - put the data from fruityvice.com/api into new variable named below but formats it.
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
