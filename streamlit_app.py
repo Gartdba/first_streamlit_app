fa95e1ab52ea25a4a8d32f46055ffa6ee1e3d0ae
Gartdba <147558955+Gartdba@users.noreply.github.com>
2023-10-26 21:27:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,6 +54,9 @@ if fruit_choice:  # Check if the input is not empty
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
 
+# we don't want to run anything past here while we toubleshoot - lab 12
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 ##my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
